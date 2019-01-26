d30de55a05a1d28bcf019e33d977906ff6c8efbd
Fernando Marcheti <fernandomarcheti@gmail.com>
2019-01-26 14:31:13 -0200

    Criacao estrutura de App Bar Search

--- app/src/main/java/com/example/githubissuessearch/MainActivity.kt
+++ app/src/main/java/com/example/githubissuessearch/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.githubissuessearch
 
-import android.support.v7.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handleIntent(intent)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search)?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+        }
+    }
+
 }
