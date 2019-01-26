a7e56fea676a4b289e743e0d76e2855c6eeeabbf
Fernando Marcheti <fernandomarcheti@gmail.com>
2019-01-26 17:09:12 -0200

    Criacao da chamada para a api do issues

--- app/src/main/java/com/example/githubissuessearch/ui/issue/IssueListViewModel.kt
+++ app/src/main/java/com/example/githubissuessearch/ui/issue/IssueListViewModel.kt
@@ -2,6 +2,9 @@ package com.example.githubissuessearch.ui.issue
 
 import com.example.githubissuessearch.base.BaseViewModel
 import com.example.githubissuessearch.network.GithubApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class IssueListViewModel : BaseViewModel() {
@@ -9,4 +12,42 @@ class IssueListViewModel : BaseViewModel() {
     @Inject
     lateinit var postApi: GithubApi
 
+    private lateinit var subscription: Disposable
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
+
+    init{
+        loadPosts()
+    }
+
+    private fun loadPosts(){
+        subscription = postApi.getIssues()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { onRetrievePostListSuccess() },
+                { onRetrievePostListError() }
+            )
+    }
+
+    private fun onRetrievePostListStart(){
+
+    }
+
+    private fun onRetrievePostListFinish(){
+
+    }
+
+    private fun onRetrievePostListSuccess(){
+
+    }
+
+    private fun onRetrievePostListError(){
+
+    }
 }
